6191e316dc4123e1feb72250f61a3e2bfdfbfc9b
john <tet@gmail.com>
2021-04-26 18:20:27 +0800

    基本输入流

--- hwdong/01.cpp
+++ hwdong/01.cpp
@@ -3,8 +3,36 @@
 //
 
 
-#include <cstdio>
 
-int main(){
+
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <malloc.h>
+
+#if 0
+int main() {
+
     printf("hello\n");
+
+    double x = 3.14;
+    printf("%lf %lf\n ", sqrt(x), sin(x));
+
+    char s[10] = "d hello";
+    puts(s);
+
+    char s2[16];
+    strcpy(s2, s);
+    puts(s2);
+    strcat(s2, "sdfsdf");
+    puts(s2);
+
+    printf("%d  %d \n", strlen(s), strlen(s2));
+//#else
+    char *s = (char *) (malloc(12 * sizeof(char)));
+    strcpy(s,"hello world!\n");
+    puts(s);
 }
+
+
+#endif
---
+++ hwdong/02.cpp
@@ -0,0 +1,69 @@
+//
+// Created by mc on 21-4-26.
+//
+
+
+
+#if 0
+
+#include <iostream>
+
+using namespace std;
+using std::cout;
+
+int main() {
+    std::cout << "hello world!\n";
+    std::cout << 3 + 4 << std::endl;
+
+
+    cout << "hello world2!\n";
+    cout << 4 + 4 << std::endl;
+    cout<<"https://a.hwdong.com"<<endl;
+
+
+    double  radius;
+    std::cin>>radius; //标准输入流对象cin输入运算符
+    cout<<3.14*radius*radius;
+}
+
+#elif 0
+
+using namespace std;
+
+void help();
+
+int main() {
+    while (1) {
+        help();
+        double a, b;
+        char op;
+        cin >> a >> op >> b;
+        if (op == '+') {
+            cout<<a + b <<endl;
+        }
+    }
+}
+
+void help() {
+    cout << "=============简单计算器=============\n";
+    cout << "请输入:左运算符 运算符  右运算符\n";
+}
+
+#else
+
+#include <fstream>
+#include <iostream>
+using std::cout;
+using namespace std;
+int main() {
+    ofstream oF("test.txt");
+    oF<<3.14 << "  "<<"hello woffrld\n";
+    oF.close();
+    ifstream iF("test.txt");
+    double d;
+    string str;
+    iF >> d >>str;
+    cout<<d<<" "<<str<<endl;
+}
+
+#endif

9cdb3fd94f89ccb6d5f47054449d2425441acc53
Jon <huaiyiheyuan@gmail.com>
2024-06-08 19:42:32 +0800

    hello wrold

---
+++ cherno/Log.cpp
@@ -0,0 +1,10 @@
+//
+// Created by m on 6/8/24.
+//
+
+#include <iostream>
+#include "Log.h"
+
+void Log(const char* message){
+    std::cout << message <<std::endl;
+}
---
+++ cherno/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by m on 6/8/24.
+//
+
+#include <iostream>
+#include "Log.h"
+
+void Log(const char* message);
+
+
+int main(){
+    Log("hello world");
+    std::cin.get();
+}
